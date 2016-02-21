342fc27c2e171d3fc871b0c6f667fe1dcfaa9d69
Markus Beier <Markus Beier>
2016-02-21 22:18:25 +0100

    Added ip-whitelist_example.php file in /app/functions.

---
+++ app/functions/ip-whitelist_example.php
@@ -0,0 +1,11 @@
+<?php
+  if($_SERVER['HTTP_HOST'] != "localhost")
+	die("Access prohibited!");
+
+  $whitelist = array(
+	''		// add as many IPs you need in this array
+  );
+  for($i=0; $i < sizeof($whitelist); $i++){
+	echo $whitelist[$i] . '<br />';
+  }
+?>
