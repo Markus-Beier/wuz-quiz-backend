1691468619db57ed6ccc926a954c1ea5340f0492
Markus Beier <Markus Beier>
2016-02-25 15:57:49 +0100

    Security update.

--- app/functions/live-table-edit.php
+++ app/functions/live-table-edit.php
@@ -1,8 +1,8 @@
 <?php
-  if(in_array($_SERVER['REMOTE_ADDR'], explode ('<br />', file_get_contents('http://localhost/functions/ip-whitelist.php')))){	// allowed IP-addresses to access the Admin-Configuration
+  session_start();
+  if ($_SESSION['username'] != NULL){
+	echo 'test';
 	require_once('../functions.inc.php');
-
-<<<<<<< HEAD
 	if($_GET['id'] AND $_GET['data'] AND $_GET['table'] AND $_GET['key']){
 	  $id = $_GET['id'];
 	  $data = $_GET['data'];
@@ -10,18 +10,6 @@
 	  $key = $_GET['key'];
 	  $result = (new db)->query('UPDATE ' . $table . ' SET ' . $key . '=\'' . $data . '\' WHERE id=\'' . $id . '\';');
 	  if($result)
-=======
-if($_SERVER['HTTP_HOST'] != "localhost")
-	die("Access prohibited!");
-
-if($_GET['id'] AND $_GET['data'] AND $_GET['table'] AND $_GET['key']){
-	$id = $_GET['id'];
-	$data = $_GET['data'];
-	$table = $_GET['table'];
-	$key = $_GET['key'];
-	$result = (new db)->query('UPDATE ' . $table . ' SET ' . $key . '=\'' . $data . '\' WHERE id=\'' . $id . '\';');
-	if($result)
->>>>>>> 26ce777d4f125ebafafdd4f1f941423fb58726bd
 		echo 'success';
 	}
   }else{
--- app/sites/admin.php/admin.php
+++ app/sites/admin.php/admin.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once('functions.inc.php');
 
 $behavior = new loginCheck;
