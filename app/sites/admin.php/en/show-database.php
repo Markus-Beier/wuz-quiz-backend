fc56c55f654ec1d696cc0a156bd89104e451d271
Markus Beier <Markus Beier>
2016-03-06 16:36:37 +0100

    Added db table board

--- app/sites/admin.php/en/show-database.php
+++ app/sites/admin.php/en/show-database.php
@@ -10,27 +10,36 @@
 														'context' => 'success',
 														'title'   => 'Well done!',
 														'dismissible' => true
+														),
+	'table_structure_created_board' => array(			'message' => 'Table structure for `board` successfully created!',
+														'context' => 'success',
+														'title'   => 'Well done!',
+														'dismissible' => true
 														)
   );
 ?>
 
-		<div class="container">
-		  <div class="jumbotron">
+		<div class='container'>
+		  <div class='jumbotron'>
 			<h2>Quiz-Overview:</h2>
 			<p>This interface shows editable quizzes.</p>
-			<table class= "table table-striped table-bordered table-hover dataTable" id="datatable">
+			<table class= 'table table-striped table-bordered table-hover dataTable' id="datatable">
 			  <thead>
-			    <tr>
-				  <th colspan="7" rowspan="1"  tabindex="0">table "question"</th>
+				<tr>
+				  <th colspan='7' rowspan='1'  tabindex='0'>table "question"</th>
 				</tr>
 				<tr>
-				  <th colspan="1" rowspan="1"  tabindex="0"><span class='glyphicon glyphicon-cog'></span></th>
-				  <th colspan="1" rowspan="1"  tabindex="0">id<br />[SMALLINT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">question<br />[TINYTEXT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">type<br />[TINYINT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">creation<br />[TIMESTAMP]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">board<br />[TINYINT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">active<br />[BOOLEAN]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>
+					<span style='width: 100%; height: 100%; text-align: center; line-height: 3em; vertical-align: middle;'
+					class='glyphicon glyphicon-cog'>
+					</span>
+				  </th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>id<br />[SMALLINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>question<br />[TINYTEXT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>type<br />[TINYINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>creation<br />[TIMESTAMP]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>board<br />[TINYINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>active<br />[BOOLEAN]</th>
 				  </tr>
 			  </thead>
 			  <tbody>
@@ -53,30 +62,30 @@
 					$result = (new db)->query('SELECT * FROM `question`');
 					while($row = $result->fetch_assoc()){
 						echo'
-						<tr>
-							<td><a class="btn btn-danger"><span class=\'glyphicon glyphicon-trash\'></span></a></td>
-							<td>'.$row['id'].'</td>
-							<td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'question\' key=\'question\'>' . $row['question'] . '</span></td>
-							<td><span class=\'xedit-dropdown-type\' id=\'' . $row['id'] . '\' table=\'question\' key=\'type\'>' . $row['type'] . '</span></td>
-							<td>' . $row['creation'] . '</td>
-							<td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'question\' key=\'board\'>' . $row['board'] . '</span></td>
-							<td><span class=\'xedit-dropdown-active\' id=\'' . $row['id'] . '\' table=\'question\' key=\'active\'>' . $row['active'] . '</span></td>
-						</tr>';
+				<tr>
+				  <td><a class=\'btn btn-danger\'><span class=\'glyphicon glyphicon-trash\'></span></a></td>
+				  <td>'.$row['id'].'</td>
+				  <td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'question\' key=\'question\'>' . $row['question'] . '</span></td>
+				  <td><span class=\'xedit-dropdown-type\' id=\'' . $row['id'] . '\' table=\'question\' key=\'type\'>' . $row['type'] . '</span></td>
+				  <td>' . $row['creation'] . '</td>
+				  <td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'question\' key=\'board\'>' . $row['board'] . '</span></td>
+				  <td><span class=\'xedit-dropdown-active\' id=\'' . $row['id'] . '\' table=\'question\' key=\'active\'>' . $row['active'] . '</span></td>
+				</tr>';
 					}
-				?>
+				?>	
 			  </tbody>
 			</table>
 			<table class= "table table-striped table-bordered table-hover dataTable" id="datatable">
 			  <thead>
-			    <tr>
-				  <th colspan="4" rowspan="1"  tabindex="0">table "answer_choice"</th>
+				<tr>
+				  <th colspan='4' rowspan='1'  tabindex='0'>table "answer_choice"</th>
 				</tr>
 				<tr>
-				  <th colspan="1" rowspan="1"  tabindex="0">id<br />[SMALLINT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">question_id<br />[SMALLINT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">answer<br />[TINYTEXT]</th>
-				  <th colspan="1" rowspan="1"  tabindex="0">correct<br />[BOOLEAN]</th>
-				  </tr>
+				  <th colspan='1' rowspan='1'  tabindex='0'>id<br />[SMALLINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>question_id<br />[SMALLINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>answer<br />[TINYTEXT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>correct<br />[BOOLEAN]</th>
+				</tr>
 			  </thead>
 			  <tbody>
 				<?php
@@ -96,14 +105,48 @@
 					$result = (new db)->query('SELECT * FROM `answer_choice`');
 					while($row = $result->fetch_assoc()){
 						echo'
-						<tr>
-							<td>'.$row['id'].'</td>
-							<td>'.$row['question_id'].'</td>
-							<td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'answer_choice\' key=\'answer\'>' . $row['answer'] . '</span></td>
-							<td><span class=\'xedit-dropdown-correct\' id=\'' . $row['id'] . '\' table=\'answer_choice\' key=\'correct\'>' . $row['correct'] . '</span></td>
-						</tr>';
+				<tr>
+				  <td>'.$row['id'].'</td>
+				  <td>'.$row['question_id'].'</td>
+				  <td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'answer_choice\' key=\'answer\'>' . $row['answer'] . '</span></td>
+				  <td><span class=\'xedit-dropdown-correct\' id=\'' . $row['id'] . '\' table=\'answer_choice\' key=\'correct\'>' . $row['correct'] . '</span></td>
+				</tr>';
 					}
-				?>
+				?>	
+			  </tbody>
+			</table>
+			<table class= "table table-striped table-bordered table-hover dataTable" id="datatable">
+			  <thead>
+				<tr>
+				  <th colspan='2' rowspan='1'  tabindex='0'>table "board"</th>
+				</tr>
+				<tr>
+				  <th colspan='1' rowspan='1'  tabindex='0'>id<br />[SMALLINT]</th>
+				  <th colspan='1' rowspan='1'  tabindex='0'>title<br />[TINYTEXT]</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<?php
+					if (!(new db)->query('DESCRIBE `board`')){
+					  echo(bootstrap_alert($alerts, 'table_structure_created_board'));
+					  // Create table structure wuz.board
+					  (new db)->query('
+						CREATE TABLE IF NOT EXISTS `board` (
+						  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+						  `title` tinytext CHARACTER SET utf8 NOT NULL,
+						  PRIMARY KEY (`id`)
+						) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+					  ');
+					}
+					$result = (new db)->query('SELECT * FROM `board`');
+					while($row = $result->fetch_assoc()){
+						echo'
+				<tr>
+				  <td>'.$row['id'].'</td>
+				  <td><span class=\'xedit-text\' id=\'' . $row['id'] . '\' table=\'board\' key=\'title\'>' . $row['title'] . '</span></td>
+				</tr>';
+					}
+				?>	
 			  </tbody>
 			</table>
 		  </div>
