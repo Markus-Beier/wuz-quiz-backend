acb4f3b0c18560eef9c16162207f2bc00b50da7e
Markus Beier <Markus Beier>
2016-03-06 17:29:21 +0100

    Translation and extended add-question.

--- app/sites/admin.php/de/add-a-question.php
+++ app/sites/admin.php/de/add-a-question.php
@@ -8,19 +8,31 @@
 				<!-- Form Name -->
 				<legend>Quiz-Frage erstellen</legend>
 
+				<!-- Select Basic -->
+				<div class="form-group">
+				  <label class="col-md-4 control-label" for="selectbasic_board">Tafel/Quiz auswählen:</label>
+				  <div class="col-md-5">
+					<select id="selectbasic_board" name="selectbasic_board" class="form-control">
+					  <option value="1">Erstes Board</option>
+					  <option value="2">2 - Die Anfänge</option>
+					</select>
+				  </div>
+				</div>
+
 				<!-- Text input-->
 				<div class="form-group">
 				  <label class="col-md-4 control-label" for="textinput_question">Frage:</label>  
 				  <div class="col-md-8">
-				  <input id="textinput" name="textinput" type="text" placeholder="Vor wie vielen Jahren wurde in Ostfriesland schon Ackerbau betrieben?" class="form-control input-md" required="">
+				  <input id="textinput_question" name="textinput_question" type="text"
+				  placeholder="Vor wie vielen Jahren wurde in Ostfriesland schon Ackerbau betrieben?" class="form-control input-md" required="" />
 				  </div>
 				</div>
 
 				<!-- File Button --> 
 				<div class="form-group">
-				  <label class="col-md-4 control-label" for="filebutton">Foto hochladen:</label>
+				  <label class="col-md-4 control-label" for="filebutton_img">Foto hochladen:</label>
 				  <div class="col-md-4">
-					<input id="filebutton" name="filebutton" class="input-file" type="file">
+					<input id="filebutton_img" name="filebutton_img" class="input-file" type="file" />
 				  </div>
 				</div>
 
@@ -32,7 +44,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox" checked="checked">     
 					  </span>
-					  <input id="prependedcheckbox_0" name="prependedcheckbox_0" class="form-control" type="text" placeholder="Vor 2.500 Jahren" required="">
+					  <input id="prependedcheckbox_0" name="prependedcheckbox_0" class="form-control" type="text"
+					  placeholder="Vor 2.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -45,7 +58,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_1" name="prependedcheckbox_1" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_1" name="prependedcheckbox_1" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -58,7 +72,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_2" name="prependedcheckbox_2" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_2" name="prependedcheckbox_2" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -71,16 +86,17 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_3" name="prependedcheckbox_3" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_3" name="prependedcheckbox_3" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
 
 				<!-- Button -->
 				<div class="form-group">
-				  <label class="col-md-4 control-label" for="singlebutton"></label>
+				  <label class="col-md-4 control-label" for="singlebutton_submit"></label>
 				  <div class="col-md-4">
-					<button id="singlebutton" name="singlebutton" class="btn btn-success">speichern</button>
+					<button id="singlebutton_submit" name="singlebutton_submit" class="btn btn-success">speichern</button>
 				  </div>
 				</div>
 
--- app/sites/admin.php/en/add-a-question.php
+++ app/sites/admin.php/en/add-a-question.php
@@ -8,19 +8,31 @@
 				<!-- Form Name -->
 				<legend>Create quiz-question</legend>
 
+				<!-- Select Basic -->
+				<div class="form-group">
+				  <label class="col-md-4 control-label" for="selectbasic_board">Select board:</label>
+				  <div class="col-md-5">
+					<select id="selectbasic_board" name="selectbasic_board" class="form-control">
+					  <option value="1">Erstes Board</option>
+					  <option value="2">2 - Die Anfänge</option>
+					</select>
+				  </div>
+				</div>
+
 				<!-- Text input-->
 				<div class="form-group">
 				  <label class="col-md-4 control-label" for="textinput_question">Question:</label>  
 				  <div class="col-md-8">
-				  <input id="textinput" name="textinput" type="text" placeholder="Vor wie vielen Jahren wurde in Ostfriesland schon Ackerbau betrieben?" class="form-control input-md" required="">
+				  <input id="textinput_question" name="textinput_question" type="text"
+				  placeholder="Vor wie vielen Jahren wurde in Ostfriesland schon Ackerbau betrieben?" class="form-control input-md" required="" />
 				  </div>
 				</div>
 
 				<!-- File Button --> 
 				<div class="form-group">
-				  <label class="col-md-4 control-label" for="filebutton">Upload photo:</label>
+				  <label class="col-md-4 control-label" for="filebutton_img">Upload photo:</label>
 				  <div class="col-md-4">
-					<input id="filebutton" name="filebutton" class="input-file" type="file">
+					<input id="filebutton_img" name="filebutton_img" class="input-file" type="file" />
 				  </div>
 				</div>
 
@@ -32,7 +44,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox" checked="checked">     
 					  </span>
-					  <input id="prependedcheckbox_0" name="prependedcheckbox_0" class="form-control" type="text" placeholder="Vor 2.500 Jahren" required="">
+					  <input id="prependedcheckbox_0" name="prependedcheckbox_0" class="form-control" type="text"
+					  placeholder="Vor 2.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -45,7 +58,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_1" name="prependedcheckbox_1" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_1" name="prependedcheckbox_1" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -58,7 +72,8 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_2" name="prependedcheckbox_2" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_2" name="prependedcheckbox_2" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
@@ -71,16 +86,17 @@
 					  <span class="input-group-addon">     
 						  <input type="checkbox">     
 					  </span>
-					  <input id="prependedcheckbox_3" name="prependedcheckbox_3" class="form-control" type="text" placeholder="Vor 1.500 Jahren" required="">
+					  <input id="prependedcheckbox_3" name="prependedcheckbox_3" class="form-control" type="text"
+					  placeholder="Vor 1.500 Jahren" required="" />
 					</div>
 				  </div>
 				</div>
 
 				<!-- Button -->
 				<div class="form-group">
-				  <label class="col-md-4 control-label" for="singlebutton"></label>
+				  <label class="col-md-4 control-label" for="singlebutton_submit"></label>
 				  <div class="col-md-4">
-					<button id="singlebutton" name="singlebutton" class="btn btn-success">submit</button>
+					<button id="singlebutton_submit" name="singlebutton_submit" class="btn btn-success">submit</button>
 				  </div>
 				</div>
 
