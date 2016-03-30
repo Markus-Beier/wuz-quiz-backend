f0b1737ff5b0751dd7f8d0ae9e626d4e4bd13630
Markus-Beier <Markus-Beier@users.noreply.github.com>
2016-03-31 01:36:11 +0200

    Quizmanager updated.

--- app/sites/admin.php/de/manage-questions.php
+++ app/sites/admin.php/de/manage-questions.php
@@ -1,49 +1,42 @@
-		<div class="container">
-			<div class="jumbatron">
+		<div class='container'>
+			<div class='jumbotron'>
 				<h2>Verwalten Sie ihre Quiz-Fragen:</h2>
 				<p>In diesem Interface werden Quiz-Fragen verwaltet.</p>
 
-				<form class="form-horizontal" method="GET">
+				<form class='form-horizontal' method='GET'>
 					<?php
 						$first = 1;
 						if (isset($_GET['lang'])) {
-							echo '<input type="hidden" name="lang" value="'. $_GET['lang'] .'">';
+							echo '<input type=\'hidden\' name=\'lang\' value=\'' . $_GET['lang'] . '\'>';
 						}
 						if (isset($_GET['action'])) {
-							echo '<input type="hidden" name="action" value="'. $_GET['action'] .'">';
+							echo '<input type=\'hidden\' name=\'action\' value=\'' . $_GET['action'] . '\'>';
 						}
 			 		?>
-			 		<select id="selectbasic_board" name="choosen_quiz">
+			 		<select id='selectbasic_board' name='manage_quiz'>
 						<?php
-							$db = new db;
-
-							$boards = $db->query("SELECT * FROM `board` WHERE 1");
-							while ($row = mysqli_fetch_array($boards)) {
-								echo "<pre>Boards - ";
-								print_r($row);
-								echo "</pre>";
-
-								if (isset($_GET['choosen_quiz'])) {
-									if ($row['id'] == $_GET['choosen_quiz']) {
-										$active = '  selected="selected"';
+							$boards = (new db)->query('SELECT * FROM `board`');
+							while ($row = $boards->fetch_assoc()) {
+								if (isset($_GET['manage_quiz'])) {
+									if ($row['id'] == $_GET['manage_quiz']) {
+										$active = 'selected=\'\'';
 									} else {
-										$active = "";
+										$active = '';
 									}
 								} else {
-									$active="";
+									$active = '';
 								}
-
-								echo '<option value="'. $row['id'] .'"'. $active .'>'. $row['title'] .'</option>';
+								echo '<option value=\'' . $row['id'] .'\'' . $active . '>' . $row['title'] . '</option>';
 							}
 						?>
 					</select>
-					<input class="btn btn-success" type="submit" value="Quiz zur Verwaltung öffnen">
+					<input class='btn btn-success' type='submit' value='Quiz zur Verwaltung öffnen'>
 				</form>
 			</div>
-			<div class="well">
+			<div class='well'>
 				
-				<!--<div class="table-responsive">
-					<table class="table">
+				<!--<div class='table-responsive'>
+					<table class='table'>
 						<tr>
 							<td>#</td>
 							<td>Frage</td>
@@ -56,50 +49,48 @@
 				</div>-->
 
 				<?php
-				if (isset($_GET['choosen_quiz'])) {
-					$boards = $db->query("SELECT * FROM `board` WHERE `id`='". $_GET['choosen_quiz'] ."'");
-					$board_name = mysqli_fetch_array($boards)['title'];
-					echo "<p>Sie verwalten momentan das Quiz <b>". $board_name ."</b></p>";
-					$db = new db;
+				if (isset($_GET['manage_quiz'])) {
+					$boards = (new db)->query('SELECT * FROM `board` WHERE `id`=\'' . $_GET['manage_quiz'] . '\'');
+					$board_name = $boards->fetch_assoc()['title'];
+					echo '<p>Sie verwalten momentan das Quiz <b>' . $board_name . '</b></p>';
 
-					$boards = $db->query("SELECT * FROM `question` WHERE `board`='". $_GET['choosen_quiz'] ."'");
-					while ($row = mysqli_fetch_array($boards)) {
+					$boards = (new db)->query('SELECT * FROM `question` WHERE `board`=\'' . $_GET['manage_quiz'] . '\'');
+					while ($row = $boards->fetch_assoc()) {
 						if ($row['type'] == 0) {
-							$type="Normale Frage";
+							$type = 'Normale Frage';
 						} else {
-							$type = "<b>FEHLER!</b>";
+							$type = '<b>Question type error!</b>';
 						}
 
+						$active = '';
 						if ($row['active'] == 0) {
-							$active = " - <b>deaktiviert</b>";
-						} else {
-							$active = "";
+							$active = ' - <b>deaktiviert</b>';
 						}
 
-						$fragetext = '#' . $row['id'] . ' | ' . $row['question'] . ' - ' . $type . $active . ' | ' . $row['creation'];
-						echo '<div class="input-group">
-								<span class="input-group-btn">
-									<button class="btn btn-default" type="button"><i class="glyphicon glyphicon-pencil"></i>Bearbeiten</button>
+						$questiontext = '#' . $row['id'] . ' | ' . $row['question'] . ' - ' . $type . $active . ' | ' . $row['creation'];
+						echo '<div class=\'input-group\'>
+								<span class=\'input-group-btn\'>
+									<button class=\'btn btn-default\' type=\'button\'><i class=\'glyphicon glyphicon-pencil\'></i>Bearbeiten</button>
 								</span>
-								<span class="input-group-addon form-control" id="einfaches-addon1" style="text-align: left;">'. $fragetext .'</span>
-								<span class="input-group-btn">
-									<button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i>Löschen</button>
+								<span class=\'input-group-addon form-control\' id=\'einfaches-addon1\' style=\'text-align: left;\'>'. $questiontext .'</span>
+								<span class=\'input-group-btn\'>
+									<button class=\'btn btn-danger\' type=\'button\'><i class=\'glyphicon glyphicon-remove\'></i>Löschen</button>
 								</span>
 							</div>';
 					}
 				} else {
-					echo "<p>Bitte wählen Sie ein Quiz zum verwalten aus.</p>";
+					echo '<p>Bitte wählen Sie ein Quiz zum Verwalten aus.</p>';
 				}
 				?>
 
 				<!--
-				<div class="input-group">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="button"><i class="glyphicon glyphicon-pencil"></i>Bearbeiten</button>
+				<div class='input-group'>
+					<span class='input-group-btn'>
+						<button class='btn btn-default' type='button'><i class='glyphicon glyphicon-pencil'></i>Bearbeiten</button>
 					</span>
-					<span class="input-group-addon form-control" id="einfaches-addon1" style="text-align: left;">Frage? - Aktiv - Art - Erstellungsdatum</span>
-					<span class="input-group-btn">
-						<button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i>Löschen</button>
+					<span class='input-group-addon form-control' id='einfaches-addon1' style='text-align: left;'>Frage? - Aktiv - Art - Erstellungsdatum</span>
+					<span class='input-group-btn'>
+						<button class='btn btn-danger' type='button'><i class='glyphicon glyphicon-remove'></i>Löschen</button>
 					</span>
 				</div>
 				-->
