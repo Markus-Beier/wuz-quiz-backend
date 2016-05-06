acbc0ada9f93066511e73c88c1df2efae7e451ad
Markus-Beier <Markus-Beier@users.noreply.github.com>
2016-05-06 19:33:43 +0200

    Updated config.inc.php.

--- app/config.inc_example.php
+++ app/config.inc_example.php
@@ -1,12 +1,20 @@
 <?php
-  $db_ip						= '';
-  $db_user						= '';
-  $db_pw						= '';
-  $db_db						= '';
 
-  $g_recaptcha_data_sitekey		= '';
-  $g_recaptcha_secret			= '';
+/* DATABASE */
+  define('DB_IP',						'127.0.0.1');
+  define('DB_USER',						'');
+  define('DB_PW', 						'');
+  define('DB_DB',						'');
 
-  $login_username				= '';
-  $login_pw						= '';
+/* GENERAL */
+  define('LANG',						'de');
+
+/* LOGIN CONFIGURATION */
+  $login_username				= 		'';
+  $login_pw						= 		'';
+
+/* LOGIN SECURITY WITH GOOGLE'S reCAPTCHA */
+  define('G_RECAPTCHA_DATA_SITEKEY',	'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+  define('G_RECAPTCHA_SECRET',			'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
 ?>
