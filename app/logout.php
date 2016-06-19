b42826bdd0de63715f64ebde4960e612491257fd
Markus-Beier <Markus-Beier@users.noreply.github.com>
2016-06-19 22:52:28 +0200

    Fixed install dir issue.

--- app/logout.php
+++ app/logout.php
@@ -1,7 +1,9 @@
 <?php
+require_once('config.inc.php');
+
 	session_start();
 	$_SESSION = array();
 	session_destroy();
 	echo 'Logged out successfully!';
-	echo '<meta http-equiv=\'refresh\' content=\'2; URL=/\'>';
+	echo '<meta http-equiv=\'refresh\' content=\'2; URL=' . INSTALL_DIR . '\'>';
 ?>
